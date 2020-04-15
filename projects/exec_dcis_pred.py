e15303b0104164f51509395c8a6c12b6ee222691
Madelon Voets <mm.voets@nki.nl>
2020-04-15 09:15:49 +0200

    classification metrics cant handle continuous targets

--- projects/exec_dcis_pred.py
+++ projects/exec_dcis_pred.py
@@ -227,16 +227,16 @@ def evaluate(args, epoch, model, data_loader, writer, exp_path, return_losses=Fa
 
     metric_dict = {'DevLoss': torch.tensor(np.mean(losses)).to(args.device),
                    'DevDice': torch.tensor(np.mean(dices)).to(args.device)}
-    import pdb
-    pdb.set_trace()
+
     # Compute metrics for stored output:
     if use_classifier:
         grab_idx = 1
         outputs = torch.stack([_[grab_idx][:,1] for _ in stored_outputs]).cpu().numpy()
         gtrs = torch.stack([_[grab_idx] for _ in stored_groundtruths]).cpu().numpy()
         auc = roc_auc_score(gtrs, outputs)
-        balanced_accuracy = balanced_accuracy_score(gtrs, outputs, sample_weight=None, adjusted=False)
-        f1_score_val = f1_score(gtrs, outputs)
+        outputs_pred = (outputs > 0.5)
+        balanced_accuracy = balanced_accuracy_score(gtrs, outputs_pred, sample_weight=None, adjusted=False)
+        f1_score_val = f1_score(gtrs, outputs_pred)
         metric_dict['DevAUC'] = torch.tensor(auc).to(args.device)
         metric_dict['DevBalancedAcc'] = torch.tensor(balanced_accuracy).to(args.device)
         metric_dict['DevF1Score'] = torch.tensor(f1_score_val).to(args.device)
